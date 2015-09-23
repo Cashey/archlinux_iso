215fd25acb1d5ce8622f407fd9f5a94568b4631c
Cashey <635976510@qq.com>
2015-09-23 20:17:32 +0800

    bug fix

--- archlinux/busybox/InitArch.sh
+++ archlinux/busybox/InitArch.sh
@@ -3,20 +3,54 @@ ISO_PATH="${SD_CARD}/archlinux.iso"
 FS_TYPE="ext3"
 MOUNT="busybox mount"
 CHROOT="busybox chroot"
-MOUNT_POINT="${SD_CARD}/mnt/archlinux/"
+MOUNT_POINT="${SD_CARD}/mnt/archlinux"
 
 if [ ! -f "${MOUNT_POINT}/bin/bash" ]
 then
     echo "Mounting image ... "
-    mkdir -p "${MOUNT_POINT}" && \
-    ${MOUNT} -t ${FS_TYPE} -o loop "${ISO_PATH}" "${MOUNT_POINT}" && \
-    ${MOUNT} -o bind /dev "${MOUNT_POINT}"/dev && \
-    ${MOUNT} -o bind /proc "${MOUNT_POINT}"/proc && \
-    ${MOUNT} -o bind /sys "${MOUNT_POINT}"/sys
-    
-    if [[ $? -ne 0 ]]
+    mkdir -p "${MOUNT_POINT}"
+
+    ${MOUNT} -t ${FS_TYPE} -o loop "${ISO_PATH}" "${MOUNT_POINT}"
+    if [ $? -ne 0 ]
     then
-        echo "Mount fail, quit."
+        echo "Mount / fail, quit."
+        exit 1
+    fi
+
+    ${MOUNT} -o bind /dev "${MOUNT_POINT}"/dev
+    if [ $? -ne 0 ]
+    then
+        echo "Mount /dev fail, quit."
+        exit 1
+    fi
+
+    mkdir -p "${MOUNT_POINT}"/dev/pts
+    ${MOUNT} -o bind /dev/pts "${MOUNT_POINT}"/dev/pts
+    if [ $? -ne 0 ]
+    then
+        echo "Mount /dev/pts fail, quit."
+        exit 1
+    fi
+
+    mkdir -p "${MOUNT_POINT}"/dev/shm
+    ${MOUNT} -o bind /dev/shm "${MOUNT_POINT}"/dev/shm
+    if [ $? -ne 0 ]
+    then
+        echo "Mount /dev/shm fail, quit."
+        exit 1
+    fi
+
+    ${MOUNT} -o bind /proc "${MOUNT_POINT}"/proc
+    if [ $? -ne 0 ]
+    then
+        echo "Mount /proc fail, quit."
+        exit 1
+    fi
+
+    ${MOUNT} -o bind /sys "${MOUNT_POINT}"/sys
+    if [ $? -ne 0 ]
+    then
+        echo "Mount /sys fail, quit."
         exit 1
     fi
 fi
