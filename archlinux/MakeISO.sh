3b7c074a45e6732f9aaab0301fe4c5a7fda0db80
Cashey <635976510@qq.com>
2015-09-23 13:38:17 +0800

    modified InitArch.sh for command line param, bug fix for busybox script.

--- archlinux/MakeISO.sh
+++ archlinux/MakeISO.sh
@@ -1,15 +1,32 @@
 #!/bin/bash 
 
-ROM=~/work/archlinux/roms/ArchLinuxARM-armv7-latest.tar.gz  # must be abslute path
-ISO_SIZE=800M
+if [[ $# -ne 2 ]]
+then
+    echo "
+Usage:
+  $0 rom_path iso_size
+  
+  eg:
+  $0 ./roms/ArchLinuxARM-armv7-latest.tar.gz 800
+
+  This creates an 800M image at ./iso/archlinux.iso.
+"
+
+    exit 1
+fi
+
+ROM=`readlink -e $1`
+ISO_SIZE="${2}"
 FS_TYPE=ext3
 ISO_PATH=./iso/archlinux.iso
 
 mkdir -p /mnt/archlinux
+mkdir -p `dirname ${ISO_PATH}`
 umount /mnt/archlinux 2> /dev/null
 rm -rf "${ISO_PATH}"
 
-dd if=/dev/zero of="${ISO_PATH}" bs="${ISO_SIZE}" count=1 && \
+echo "Creating image..."
+dd if=/dev/zero of="${ISO_PATH}" bs="1M" count=${ISO_SIZE} && \
 mkfs -t "${FS_TYPE}" "${ISO_PATH}" && \
 mount -o loop "${ISO_PATH}" /mnt/archlinux -t ext3 && \
 cd /mnt/archlinux && \
